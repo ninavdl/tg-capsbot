79ba702c239d04394c937ad9187370583cbc698d
Nina <git@nv.gw>
2020-03-10 16:02:21 +0100

    - SWITCH TO 'GO MOD' MODULE MANAGEMENT - REPLACE TELEBOT BY OWN FORK WHICH CAN HANDLE COMMANDS TO OTHER BOTS - HANDLE FILE CAPTIONS AND NAMES

--- MAIN.go
+++ MAIN.go
@@ -8,22 +8,15 @@ import (
 	TIME "time"
 	UNICODE "unicode"
 
-	TB "gopkg.in/tucnak/telebot.v2"
+	TB "github.com/sour-dough/telebot/v2"
 )
 
-// STRING IS A STRING
+// ALIAS STUPID LOWERCASE TYPES AND CONSTANTS
 type STRING = string
-
-// BOOL IS A BOOL
 type BOOL = bool
-
-// RUNE IS A RUNE
 type RUNE = rune
 
-// TRUE IS TRUE
 const TRUE = true
-
-// FALSE IS FALSE
 const FALSE = false
 
 // ISUPPERCASE CHECKS WHETHER THE STRING CONTAINS JUST UPPERCASE RUNES
@@ -44,8 +37,9 @@ func main() {
 
 	var ERR error
 	BOT, ERR = TB.NewBot(TB.Settings{
-		Token:  TOKEN,
-		Poller: &TB.LongPoller{Timeout: 10 * TIME.Second},
+		Token:                   TOKEN,
+		Poller:                  &TB.LongPoller{Timeout: 10 * TIME.Second},
+		HandleCommandsForOthers: true,
 	})
 
 	if ERR != nil {
@@ -53,36 +47,77 @@ func main() {
 		return
 	}
 
+	TEXTHANDLER := FILTER(FILTERTEXT)
+	MEDIAHANDLER := FILTER(FILTERMEDIA)
+	DOCUMENTHANDLER := FILTER(FILTERDOCUMENT)
+
+	// FILTER TEXT MESSAGES TEXTS
 	BOT.Handle(TB.OnText, TEXTHANDLER)
 	BOT.Handle(TB.OnEdited, TEXTHANDLER)
+
+	// FILTER MEDIA MESSAGE'S CAPTIONS
+	BOT.Handle(TB.OnPhoto, MEDIAHANDLER)
+	BOT.Handle(TB.OnAudio, MEDIAHANDLER)
+	BOT.Handle(TB.OnVideo, MEDIAHANDLER)
+
+	// FILTER DOCUMENT'S CAPTIONS AND FILE NAMES
+	BOT.Handle(TB.OnDocument, DOCUMENTHANDLER)
 
 	BOT.Start()
 }
 
-// TEXTHANDLER HANDLES TELEGRAM TEXT MESSAGES
-func TEXTHANDLER(MSG *TB.Message) {
-	if !MSG.FromGroup() || ISUPPERCASE(MSG.Text) {
-		return
+// FILTER GROUP MESSAGES BY THE GIVEN FUNCTION
+// IF THE FUNCTION RETURNS TRUE, DELETE THE MESSAGE,
+// KICK THE USER AND SEND A WARNING MESSAGE
+func FILTER(FILTERFUNC func(*TB.Message) BOOL) func(*TB.Message) {
+	return func(MSG *TB.Message) {
+		if !MSG.FromGroup() {
+			return
+		}
+
+		if !FILTERFUNC(MSG) {
+			return
+		}
+
+		MEMBER, ERR := BOT.ChatMemberOf(MSG.Chat, MSG.Sender)
+		if ERR != nil {
+			LOG.Println(ERR)
+			return
+		}
+
+		BOT.Delete(MSG)
+		var NAME STRING
+		if MSG.Sender.Username != "" {
+			NAME = "@" + MSG.Sender.Username
+		} else {
+			NAME = MSG.Sender.FirstName + " " + MSG.Sender.LastName
+		}
+
+		BOT.Send(MSG.Chat, STRINGS.ToUpper(NAME)+" RAUS")
+		// BANNING AND UNBANNING SHOULD BE EQUIVALENT TO KICKING
+		BOT.Ban(MSG.Chat, MEMBER)
+		BOT.Unban(MSG.Chat, MSG.Sender)
+
+		LOG.Printf("%s VIOLATED THE NO-LOWER-CASE RULE AND WAS PUNISHED", NAME)
 	}
+}
 
-	MEMBER, ERR := BOT.ChatMemberOf(MSG.Chat, MSG.Sender)
-	if ERR != nil {
-		LOG.Println(ERR)
-		return
+// FILTERTEXT RETURNS TRUE IFF A GIVEN TEXT MESSAGE CONTAINS LOWER-CASE LETTERS
+func FILTERTEXT(MSG *TB.Message) BOOL {
+	return !ISUPPERCASE(MSG.Text)
+}
+
+// FILTERDOCUMENT RETURNS TRUE IFF A MESSAGE'S ATTACHED FILE'S NAME OR CAPTION CONTAINS LOWER-CASE LETTERS
+func FILTERDOCUMENT(MSG *TB.Message) BOOL {
+	if MSG.Document != nil {
+		if !ISUPPERCASE(MSG.Document.FileName) {
+			return true
+		}
 	}
+	return !ISUPPERCASE(MSG.Caption)
+}
 
-	BOT.Delete(MSG)
-	var NAME STRING
-	if MSG.Sender.Username != "" {
-		NAME = "@" + MSG.Sender.Username
-	} else {
-		NAME = MSG.Sender.FirstName + " " + MSG.Sender.LastName
-	}
-
-	BOT.Send(MSG.Chat, STRINGS.ToUpper(NAME)+" RAUS")
-	// BANNING AND UNBANNING SHOULD BE EQUIVALENT TO KICKING
-	BOT.Ban(MSG.Chat, MEMBER)
-	BOT.Unban(MSG.Chat, MSG.Sender)
-
-	LOG.Printf("%s VIOLATED THE NO-LOWER-CASE RULE AND WAS PUNISHED", NAME)
+// FILTERMEDIA RETURNS TRUE IFF A GIVEN MEDIA MESSAGE'S CAPTION CONTAINS LOWER-CASE LETTERS
+func FILTERMEDIA(MSG *TB.Message) BOOL {
+	return !ISUPPERCASE(MSG.Caption)
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sour-dough/tg-capsbot
+
+go 1.14
+
+require github.com/sour-dough/telebot/v2 v2.1.1
